6d767a8575ba5759eb17963b5586e62fed9ee23a
Ettikus <141125542+Ettikus@users.noreply.github.com>
2023-08-21 14:13:10 +0200

    Update script.js
    Removed Live Scores

--- script.js
+++ script.js
@@ -28,29 +28,3 @@ function getWeather(city) {
             console.error("Error fetching weather data:", error);
             weatherInfo.innerHTML = "Error fetching weather data.";
         });
-}
-function getLiveScores() {
-    const apiUrl = "apiv3.apifootball.com/?action=get_leaguesT"; // Replace with your API endpoint
-    const apiKey = "6f0c36d842ecad9f5db52a3c9cf43acb947634c5dc5fe9dd3b7495deb1200034"; // Replace with your API key
-
-    fetch(apiUrl, {
-        headers: {
-            "Authorization": `Bearer ${apiKey}`
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        const scores = data.scores;
-
-        const scoresList = document.getElementById("scoresList");
-        scoresList.innerHTML = scores.map(score => `<li>${score.team1} ${score.score1} - ${score.score2} ${score.team2}</li>`).join('');
-    })
-    .catch(error => {
-        console.error("Error fetching live scores:", error);
-        const scoresList = document.getElementById("scoresList");
-        scoresList.innerHTML = "Error fetching live scores.";
-    });
-}
-
-// Fetch live scores on page load
-getLiveScores();
