fee2b0a3c3173f7a8af98d5bf92c93e9e1f51f00
Ettikus <141125542+Ettikus@users.noreply.github.com>
2023-08-23 11:01:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const searchButton = document.getElementById("searchButton");
 const cityInput = document.getElementById("cityInput");
 const weatherInfo = document.getElementById("weatherInfo");
+const apiKey = "YOUR_API_KEY"; // Replace with your OpenWeatherMap API key
 
 searchButton.addEventListener("click", () => {
     const city = cityInput.value;
@@ -10,17 +11,17 @@ searchButton.addEventListener("click", () => {
 });
 
 function getWeather(city) {
-    const apiUrl = `api.openweathermap.org/data/2.5/weather?id=524901&APPID=be0f40f4dfad82fdc8eb601136fb0bd1`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${be0f40f4dfad82fdc8eb601136fb0bd1}`;
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            const temperature = data.current.temp_c;
-            const description = data.current.condition.text;
+            const temperature = data.main.temp;
+            const description = data.weather[0].description;
 
             weatherInfo.innerHTML = `
                 <h2>Weather in ${city}</h2>
-                <p>Temperature: ${temperature}°C</p>
+                <p>Temperature: ${temperature} K</p>
                 <p>Description: ${description}</p>
             `;
         })
@@ -28,3 +29,4 @@ function getWeather(city) {
             console.error("Error fetching weather data:", error);
             weatherInfo.innerHTML = "Error fetching weather data.";
         });
+}
